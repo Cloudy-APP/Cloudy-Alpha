dc974190775b1f9e178652206bc683269897777d
Mateusz <mateuszslotwinski16@gmail.com>
2021-09-05 19:04:19 +0200

    beginnings can be difficult

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+    <UploadFiles />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import UploadFiles from '../components/UploadFiles.vue'
 
 export default defineComponent({
   name: 'Home',
   components: {
+    UploadFiles
   }
 })
 </script>
